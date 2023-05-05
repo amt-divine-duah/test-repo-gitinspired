1c017e95584034e55781a66314652c52438f0c2b
Ishaque <108622096+Ishaque22@users.noreply.github.com>
2023-05-05 14:16:42 +0000

    message

--- frontend/src/components/SignoutModal.tsx
+++ frontend/src/components/SignoutModal.tsx
@@ -6,8 +6,8 @@ type Prop={
 const SignoutModal = ({handleLogout}:Prop) => {
   
   return (
-    <div className="container">
-        <div className="signOutModal">
+    <div className="container" onClick={handleLogout}>
+        <div className="signOutModal" onClick={(e)=>e.stopPropagation()}>
             <h3>Confirm Logout</h3>
             <p>Are you sure you want to logout from <span className='logoutBoldText'>Assign IT Dashboard?</span></p>
             <div className="logout-btns">
