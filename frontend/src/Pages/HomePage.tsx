791114548d17d6f745ade7648fe50b42c9b512cc
Santadd <hethere97@gmail.com>
2023-05-05 05:31:58 +0000

    fix deploy issues

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,9 +1,5 @@
-import AdminLecturerDashBoard from "../components/AdminLecturerDashBoard";
-import AdminStudentDashBoard from "../components/AdminStudentDashBoard";
 import Main from "../components/Main";
-import NavBar from "../components/NavBar"
 import '../Styles/homePage.scss'
-import {Routes, Route} from 'react-router-dom';
 
 const HomePage = () => {
   return (
