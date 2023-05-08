399978788363f080c857161a4888cfd5e0a0b840
Kayturk <kweku.gyimah-turkson@amalitech.org>
2023-05-08 11:09:11 +0000

    css changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import HomePage from "./Pages/HomePage"
 import LecturerDashBoard from "./Pages/LecturerDashBoard"
+import LoginPage from "./Pages/LoginPage"
 
 
 
 const App = () => {
   return (
     <>
-    <LecturerDashBoard/>
+     <LoginPage />
     </>
   )
 }
