6f95b8e3055fe824fd192e5395b02a341aa4359e
Kayturk <kweku.gyimah-turkson@amalitech.org>
2023-05-08 11:37:52 +0000

    css changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import HomePage from "./Pages/HomePage"
 import LecturerDashBoard from "./Pages/LecturerDashBoard"
 import LoginPage from "./Pages/LoginPage"
+import ResetPassword from "./Pages/ResetPassword"
 
 
 
 const App = () => {
   return (
     <>
-     <LoginPage />
+     <ResetPassword />
     </>
   )
 }
