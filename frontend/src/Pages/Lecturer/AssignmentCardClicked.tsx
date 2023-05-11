80879c979f8183ce7396b58d2ee7217df852b1fb
Kweku <kweku.gyimah-turkson@amalitech.org>
2023-05-11 09:28:37 +0000

    newly added tabs

---
+++ frontend/src/Pages/Lecturer/AssignmentCardClicked.tsx
@@ -0,0 +1,56 @@
+import { useLocation } from "react-router-dom";
+import ActionButton from "../../components/Lecturer/ActionButton";
+import BottomContent from "../../components/Lecturer/AssignmentClickedBottomContent";
+import TopContent from "../../components/Lecturer/AssignmentClickedTopContent";
+import Searchbar from "../../components/Lecturer/Searchbar";
+import useData from "../../hooks/useAssignmentData";
+
+const AssignmentCardClicked = () => {
+    // const keyword = useLocation();
+    // const uniqueCode: string = keyword.state.name;
+     
+    // const dataValue = useData();
+
+    
+    // const assignmentData = dataValue.assignments;
+
+    // const output = assignmentData.filter((item) => {
+    //     return item.code === uniqueCode
+    // });
+
+    const {assignments} = useData();
+     const output = assignments.filter((item) => {
+        return item.javascript === uniqueCode
+    });
+  return (
+       
+      <div className="main-content">
+        <div className="page-features">
+          <Searchbar />
+          <div className="header-right">
+          <ActionButton class={"action filter"} name={"filter by date"} />
+          <ActionButton class={"action assign"} name={"assignment +"} />
+          </div>
+        </div>
+        {/* //// */}
+        <div className="clicked-top">
+          <h2 className="clicked-header1">Assignment</h2>
+          {assignments.map((item) => {
+             return (
+                <TopContent title={item.title} uniqueCode={item.code} date={item.date} details={item.fullDetails} />
+             )
+            })}
+        
+        </div>
+        {assignments.map((item) => {
+             return (
+                <BottomContent  invitedStudents={item.numberOfStudents}/>
+             )
+            })}
+      
+        {/* //// */}
+      </div>
+  );
+};
+
+export default AssignmentCardClicked;
--- frontend/src/Pages/Lecturer/DashboardTab.tsx
+++ frontend/src/Pages/Lecturer/DashboardTab.tsx
@@ -2,32 +2,11 @@ import { useState } from "react";
 import ActionButton from "../../components/Lecturer/ActionButton";
 import AssignmentCard from "../../components/Lecturer/AssignmentCard";
 import Searchbar from "../../components/Lecturer/Searchbar";
+import useData from "../../hooks/useAssignmentData";
 
 Searchbar;
 const DashboardTab = () => {
-  const [cardData, setCardData] = useState([
-    {
-      title: "Javascript",
-      description:
-        "Your task is to develop a web application using JavaScript that provides a user-friendly interface for tracking and managing tasks. ",
-      date: "1st May, 2023",
-      uniqueCode: "5867947",
-    },
-    {
-      title: "REACT",
-      description:
-        "Build a web application using React that allows users to browse and search a collection of items.",
-      date: "1st May, 2023",
-      uniqueCode: "5867947",
-    },
-    {
-      title: "Typescript",
-      description:
-        "Develop a TypeScript program that reads input data from a JSON file and outputs the data to the console. The program should demonstrate an understanding of TypeScript syntax and data types, i",
-      date: "1st May, 2023",
-      uniqueCode: "5867947",
-    },
-  ]);
+  const {assignments} = useData();
   return (
     <div className="main-content">
       <div className="page-features">
@@ -41,9 +20,9 @@ const DashboardTab = () => {
         <h2 className="page-header">Assignment</h2>
         <div className="assignment-container">
 
-        { cardData.map((item,index) => {
+        { assignments.map((item,index) => {
           return (
-            <AssignmentCard title={item.title} description={item.description} date={item.date} uniqueCode={item.uniqueCode}/>
+            <AssignmentCard title={item.title} description={item.description} date={item.date} uniqueCode={item.code}/>
           )
         }) }
       </div>
